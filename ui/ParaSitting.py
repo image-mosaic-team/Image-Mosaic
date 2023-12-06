58cf9821690e15779c8c4b07ad5613afc4a224f6
derrb <1570697642@qq.com>
2023-12-06 10:45:18 +0800

    实现按钮打开界面

--- ui/ParaSitting.py
+++ ui/ParaSitting.py
@@ -1,13 +1,15 @@
 # @Author : fxl
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class ParaSitting(object):
-    def PS_setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(600, 400)
-        self.PushButton = QtWidgets.QPushButton(Form)
+    def PS_setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 400)
+        self.PushButton = QtWidgets.QPushButton(MainWindow)
         self.PushButton.setGeometry(QtCore.QRect(450, 70, 100, 60))
         font = QtGui.QFont()
         font.setPointSize(18)
@@ -15,7 +17,7 @@ class ParaSitting(object):
         font.setWeight(75)
         self.PushButton.setFont(font)
         self.PushButton.setObjectName("PushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2 = QtWidgets.QPushButton(MainWindow)
         self.pushButton_2.setGeometry(QtCore.QRect(450, 270, 100, 60))
         font = QtGui.QFont()
         font.setPointSize(18)
@@ -23,12 +25,12 @@ class ParaSitting(object):
         font.setWeight(75)
         self.pushButton_2.setFont(font)
         self.pushButton_2.setObjectName("pushButton_2")
-        self.line = QtWidgets.QFrame(Form)
+        self.line = QtWidgets.QFrame(MainWindow)
         self.line.setGeometry(QtCore.QRect(400, 0, 20, 400))
         self.line.setFrameShape(QtWidgets.QFrame.VLine)
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setObjectName("line")
-        self.label = QtWidgets.QLabel(Form)
+        self.label = QtWidgets.QLabel(MainWindow)
         self.label.setGeometry(QtCore.QRect(50, 50, 72, 15))
         font = QtGui.QFont()
         font.setPointSize(9)
@@ -36,20 +38,20 @@ class ParaSitting(object):
         font.setWeight(50)
         self.label.setFont(font)
         self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2 = QtWidgets.QLabel(MainWindow)
         self.label_2.setGeometry(QtCore.QRect(30, 180, 111, 15))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3 = QtWidgets.QLabel(MainWindow)
         self.label_3.setGeometry(QtCore.QRect(10, 310, 141, 15))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.label_3.setFont(font)
         self.label_3.setTextFormat(QtCore.Qt.PlainText)
         self.label_3.setObjectName("label_3")
-        self.frame = QtWidgets.QFrame(Form)
+        self.frame = QtWidgets.QFrame(MainWindow)
         self.frame.setGeometry(QtCore.QRect(200, 20, 131, 81))
         self.frame.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
@@ -63,7 +65,7 @@ class ParaSitting(object):
         self.textEdit.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.textEdit.setFrameShadow(QtWidgets.QFrame.Raised)
         self.textEdit.setObjectName("textEdit")
-        self.frame_2 = QtWidgets.QFrame(Form)
+        self.frame_2 = QtWidgets.QFrame(MainWindow)
         self.frame_2.setGeometry(QtCore.QRect(200, 160, 131, 81))
         self.frame_2.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
@@ -77,7 +79,7 @@ class ParaSitting(object):
         self.textEdit_2.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.textEdit_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.textEdit_2.setObjectName("textEdit_2")
-        self.frame_3 = QtWidgets.QFrame(Form)
+        self.frame_3 = QtWidgets.QFrame(MainWindow)
         self.frame_3.setGeometry(QtCore.QRect(200, 280, 131, 81))
         self.frame_3.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
@@ -91,29 +93,39 @@ class ParaSitting(object):
         self.textEdit_3.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.textEdit_3.setFrameShadow(QtWidgets.QFrame.Raised)
         self.textEdit_3.setObjectName("textEdit_3")
+        MainWindow.setStatusBar(self.statusbar)
 
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def PS_retranslateUi(self, Form):
+    def PS_retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.PushButton.setText(_translate("Form", "OK"))
-        self.pushButton_2.setText(_translate("Form", "EXIT"))
-        self.label.setText(_translate("Form", "Ratio"))
-        self.label_2.setText(_translate("Form", "Reprojthresh"))
-        self.label_3.setText(_translate("Form", "Homography_matrix"))
-        self.textEdit.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.PushButton.setText(_translate("MainWindow", "OK"))
+        self.pushButton_2.setText(_translate("MainWindow", "EXIT"))
+        self.label.setText(_translate("MainWindow", "Ratio"))
+        self.label_2.setText(_translate("MainWindow", "Reprojthresh"))
+        self.label_3.setText(_translate("MainWindow", "Homography_matrix"))
+        self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'SimSun\'; font-size:14pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:9pt;\"><br /></p></body></html>"))
-        self.textEdit_2.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.textEdit_2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'SimSun\'; font-size:14pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:9pt;\"><br /></p></body></html>"))
-        self.textEdit_3.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.textEdit_3.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'SimSun\'; font-size:14pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:9pt;\"><br /></p></body></html>"))
+
+
+    def start(self):
+        self.MainWindow = QMainWindow()
+        self.PS_setupUi(self.MainWindow)
+        self.PS_retranslateUi(self.MainWindow)
+        self.MainWindow.show()
+            
